ae0895ea4d54d1d65dc45ab37a1fe613d6baa91d
erickmx <erickblanc96@gmail.com>
2020-09-29 23:13:37 -0500

    implemented logic to show the number e.

--- main.go
+++ main.go
@@ -2,6 +2,16 @@ package main
 
 import "fmt"
 
-func main()  {
-	fmt.Println("Hola Mundo!") // eliminar esta línea
+func main() {
+	var numberE, acumulator float64
+	numberE = 1
+	acumulator = 1
+	for i := 1; i < 1000000; i++ {
+		for j := 2; j <= i; j++ {
+			acumulator = acumulator * float64(j)
+		}
+		numberE = numberE + (1 / acumulator)
+		acumulator = 1
+	}
+	fmt.Print(numberE)
 }

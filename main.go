712c746af3f81da6e51b5a0f0006ea95d02dd3ba
erickmx <erickblanc96@gmail.com>
2020-09-29 23:24:57 -0500

    added support to define a limit

--- main.go
+++ main.go
@@ -3,12 +3,14 @@ package main
 import "fmt"
 
 func main() {
-	var numberE, acumulator float64
+	var numberE, acumulator float32
+	var limit int
+	fmt.Scan(&limit)
 	numberE = 1
 	acumulator = 1
-	for i := 1; i < 1000000; i++ {
-		for j := 2; j <= i; j++ {
-			acumulator = acumulator * float64(j)
+	for i := 1; i <= limit; i++ {
+		for j := 1; j <= i; j++ {
+			acumulator = acumulator * float32(j)
 		}
 		numberE = numberE + (1 / acumulator)
 		acumulator = 1
